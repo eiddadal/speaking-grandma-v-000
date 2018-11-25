ed7f5c30f0ad16df5f7240c2716eb6413bf9e414
Dalya Eidda <dalia.eidda@gmail.com>
2018-11-25 17:23:23 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,11 +2,11 @@
 def speak_to_grandma(phrase)
  phrase.chomp!
   if phrase != phrase.upcase 
-     "HUH?! SPEAK UP, SONNY!" 
+    puts "HUH?! SPEAK UP, SONNY!" 
   elsif phrase== phrase.upcase 
-   "NO, NOT SINCE 1938!"
+  puts "NO, NOT SINCE 1938!"
 elsif phrase == "I LOVE YOU GRANDMA!"
-"I LOVE YOU TOO PUMPKIN!
+puts "I LOVE YOU TOO PUMPKIN!
   end 
 end 
 
