8a46c129ace3acf6c6ff0199ee3932021cba7e06
Dalya Eidda <dalia.eidda@gmail.com>
2018-11-25 17:14:38 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma(phrase)
+def speak_to_grandma(phrase.upcase)
   if phrase == phrase.upcase 
     puts "HUH?! SPEAK UP, SONNY!" 
   elsif phrase.upcase?
