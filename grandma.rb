29b31517c9ca3c28ef7c61d14bfb22e59b217ee3
Dalya Eidda <dalia.eidda@gmail.com>
2018-11-25 17:25:34 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -6,9 +6,8 @@ def speak_to_grandma(phrase)
   elsif phrase== phrase.upcase 
   return "NO, NOT SINCE 1938!"
   elsif phrase== "I LOVE YOU GRANDMA!"
-  return "I LOVE YOU TOO PUMPKIN"
-else phrase == "I LOVE YOU GRANDMA!"
-return "I LOVE YOU TOO PUMPKIN!"
+  return "I LOVE YOU TOO PUMPKIN!"
+
   end 
 end 
 
